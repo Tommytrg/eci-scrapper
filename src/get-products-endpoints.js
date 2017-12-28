2cd1585d5b0a82a94c58fcb30e3c7ee42229f9ac
Tomas <tomastrg22@gmail.com>
2017-12-28 14:08:43 +0100

    add feature get products endpoints

--- src/get-products-endpoints.js
+++ src/get-products-endpoints.js
@@ -1,29 +1,29 @@
-const _    = require('lodash')
-const fs   = require('fs')
+const _ = require('lodash')
+const fs = require('fs')
 const Xray = require('x-ray')
 
 const endpoints = require('../config/endpoints')
 const x = Xray()
 
 async function getProductEndpoints () {
-  const keys = Object.keys(endpoints);
+  const keys = Object.keys(endpoints)
   for (let key of keys) {
-    await getData(endpoints[key], key);
+    await getData(endpoints[key], key)
   }
 }
 
-function getData(endpoint, file) {
-   return x(endpoint, '.js-plp', ['.link@href'])
+function getData (endpoint, file) {
+  return x(endpoint, '.js-plp', ['.link@href'])
     .paginate('a[rel=next]@href')
-    .then(function(res) {
+    .then(function (res) {
       console.log(`Writing data from ${endpoint} in file ${file}`)
       fs.writeFileSync(`./../data/${file}.json`, JSON.stringify({endpoints: res}), 'utf-8')
     })
     .catch(function (err) {
-      console.log(`ERROR writing data from ${endpoint} in file ${file}`);
+      console.log(`ERROR writing data from ${endpoint} in file ${file}`)
       console.log(err)
     })
     .delay(2000)
-  }
-  
+}
+
 getProductEndpoints()
